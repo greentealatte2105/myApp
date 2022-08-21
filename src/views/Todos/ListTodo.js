eb308a893ba0d49ddee2a95d61e7ae3d357e6983
greentealatte2105 <20521775@ms.uit.edu.vn>
2022-08-21 15:25:32 +0700

    a

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -7,64 +7,37 @@ import { toast } from 'react-toastify';
 class ListToDo extends React.Component{
 
     state = {
-        listTodos: [
-            {id: 'todo1', title: 'Doing Homework' },
-            {id: 'todo2', title: 'Data Request' },
-            {id: 'todo3', title: 'Clean my house' },
+        listToDos: [
+            {id: 'todo1', title: 'Doing homework'},
+            {id: 'todo2', title: 'Finish task'}
         ]
     }
-
     addNewToDo = (task) => {
         this.setState(
-            {
-                listTodos: [...this.state.listTodos, task]
-            }
+            {listToDos: [...this.state.listToDos, task]}
         )
-        toast.success('Đã thêm: ' + task.title);
+        toast.success('Successfully added your task')
     }
-
-    handleEdit = () => {
-
-    }
-
-    handleDelete = (task) => {
-
-        let currentList = this.state.listTodos;
-        currentList = currentList.filter((item, index) => item.id !== task.id)
-
-        this.setState(
-           {
-            listTodos: currentList
-           }
-        )
-    }
-
     render() {
-        let {listTodos} = this.state;
-
+        let {listToDos} = this.state.listToDos;
         return(
             <div className='list-todo-container'>
-                <div>Type Here Your Task: </div>
-                <AddToDo
-                addNewToDo={this.addNewToDo}/>
+            <div>Type your Task here</div>
+            <AddToDo addNewToDo={this.addNewToDo}/>
 
                 <div className='list-todo-content'>
-                    {listTodos && listTodos.length > 0 &&
-                        listTodos.map((item, index) => {
+                    {listToDos && listToDos.length > 0 && 
+                        listToDos.map((item, index) => {
                             return(
-                                <div className='todo-child' key={item.id}>
+                               <div className='todo-child'>
                                     <span> {index + 1} - {item.title} </span>
-                                    <div className='btn'>
-                                        <button className='Edit'>Edit</button>
-                                        <button className='Delete' onClick={() => this.handleDelete(item)}>Delete</button>
-                                    </div>
-                                </div>
+                               </div>
                             )
                         })}
                 </div>
             </div>
         )
-    }   
+    }
 }   
 
 export default ListToDo;
