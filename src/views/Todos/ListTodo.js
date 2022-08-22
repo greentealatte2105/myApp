316af82ca299b05ef5cf10653b2c61f74ef5f1ee
greentealatte2105 <20521775@ms.uit.edu.vn>
2022-08-22 12:12:28 +0700

    a

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -19,20 +19,25 @@ class ListToDo extends React.Component{
         toast.success('Successfully added your task')
     }
     render() {
-        let {listToDos} = this.state.listToDos;
+        let {listToDos} = this.state;
         return(
-            <div>
-                <div className='todo-list-container'>Type your task here</div>
+            <div className='todo-list-container'>
+                <div>Type your task here </div>
                 <AddToDo addNewToDo={this.addNewToDo}/>
-                <div className='list-todo-content'>
-                    {listToDos && listToDos.length > 0 &&
-                        listToDos.map((item, index) => {    
-                            return(
-                                <div>abc</div>
-                            )
-                        })
-                    }
-                </div>
+                    <div className='list-todo-content'>
+                        {listToDos && listToDos.length > 0 &&
+                            listToDos.map((item, index) => {
+                                return (
+                                    <div className='todo-child' key={item.id}>
+                                        <span> {index + 1} - {item.title} </span>
+                                        <div className='btn'>
+                                            <button className='Edit'>Edit</button>
+                                            <button className='Delete'>Delete</button>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                    </div>
             </div>
         )
     }
